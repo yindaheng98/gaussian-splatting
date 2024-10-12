add24db4de75f322a83144f188e2f4a6211cdb9e
yindaheng98 <yindaheng98@gmail.com>
2024-10-11 22:22:33 -0700

    simple render

---
+++ gaussian_splatting/gaussian_model.py
@@ -0,0 +1,147 @@
+import math
+from typing import NamedTuple
+import torch
+from torch import nn
+
+from gaussian_splatting.diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
+
+from .utils import inverse_sigmoid, strip_symmetric, build_scaling_rotation
+
+
+class Camera(NamedTuple):
+    image_height: int
+    image_width: int
+    FoVx: float
+    FoVy: float
+    world_view_transform: torch.Tensor
+    full_proj_transform: torch.Tensor
+    camera_center: torch.Tensor
+    bg_color: list[float] = [1., 1., 1.]
+
+
+class GaussianModel(nn.Module):
+
+    def setup_functions(self):
+        def build_covariance_from_scaling_rotation(scaling, scaling_modifier, rotation):
+            L = build_scaling_rotation(scaling_modifier * scaling, rotation)
+            actual_covariance = L @ L.transpose(1, 2)
+            symm = strip_symmetric(actual_covariance)
+            return symm
+
+        self.scaling_activation = torch.exp
+        self.scaling_inverse_activation = torch.log
+
+        self.covariance_activation = build_covariance_from_scaling_rotation
+
+        self.opacity_activation = torch.sigmoid
+        self.inverse_opacity_activation = inverse_sigmoid
+
+        self.rotation_activation = torch.nn.functional.normalize
+
+    def __init__(self, sh_degree):
+        super(GaussianModel, self).__init__()
+        self.sh_degree = sh_degree
+        self._xyz = torch.empty(0)
+        self._features_dc = torch.empty(0)
+        self._features_rest = torch.empty(0)
+        self._scaling = torch.empty(0)
+        self._rotation = torch.empty(0)
+        self._opacity = torch.empty(0)
+        self.setup_functions()
+        self.scale_modifier = 1.0
+        self.debug = False
+        self.antialiasing = False
+
+    @property
+    def get_scaling(self):
+        return self.scaling_activation(self._scaling)
+
+    @property
+    def get_rotation(self):
+        return self.rotation_activation(self._rotation)
+
+    @property
+    def get_xyz(self):
+        return self._xyz
+
+    @property
+    def get_features(self):
+        features_dc = self._features_dc
+        features_rest = self._features_rest
+        return torch.cat((features_dc, features_rest), dim=1)
+
+    @property
+    def get_features_dc(self):
+        return self._features_dc
+
+    @property
+    def get_features_rest(self):
+        return self._features_rest
+
+    @property
+    def get_opacity(self):
+        return self.opacity_activation(self._opacity)
+
+    def get_covariance(self, scaling_modifier=1):
+        return self.covariance_activation(self.get_scaling, scaling_modifier, self._rotation)
+
+    def forward(self, viewpoint_camera: Camera):
+        # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+        screenspace_points = torch.zeros_like(self.get_xyz, dtype=self.get_xyz.dtype, requires_grad=True, device=self._xyz.device) + 0
+        try:
+            screenspace_points.retain_grad()
+        except:
+            pass
+
+        # Set up rasterization configuration
+        tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+        tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+        raster_settings = GaussianRasterizationSettings(
+            image_height=int(viewpoint_camera.image_height),
+            image_width=int(viewpoint_camera.image_width),
+            tanfovx=tanfovx,
+            tanfovy=tanfovy,
+            bg=torch.tensor(viewpoint_camera.bg_color, dtype=torch.float32, device=self._xyz.device),
+            scale_modifier=self.scale_modifier,
+            viewmatrix=viewpoint_camera.world_view_transform,
+            projmatrix=viewpoint_camera.full_proj_transform,
+            sh_degree=self.sh_degree,
+            campos=viewpoint_camera.camera_center,
+            prefiltered=False,
+            debug=self.debug,
+            antialiasing=self.antialiasing
+        )
+
+        rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+        means3D = self.get_xyz
+        means2D = screenspace_points
+        opacity = self.get_opacity
+
+        scales = self.get_scaling
+        rotations = self.get_rotation
+
+        shs = self.get_features
+
+        # Rasterize visible Gaussians to image, obtain their radii (on screen).
+        rendered_image, radii, depth_image = rasterizer(
+            means3D=means3D,
+            means2D=means2D,
+            shs=shs,
+            colors_precomp=None,
+            opacities=opacity,
+            scales=scales,
+            rotations=rotations,
+            cov3D_precomp=None)
+
+        # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+        # They will be excluded from value updates used in the splitting criteria.
+        rendered_image = rendered_image.clamp(0, 1)
+        out = {
+            "render": rendered_image,
+            "viewspace_points": screenspace_points,
+            "visibility_filter": (radii > 0).nonzero(),
+            "radii": radii,
+            "depth": depth_image
+        }
+        return out

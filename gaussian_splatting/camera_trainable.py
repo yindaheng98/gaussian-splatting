2a0087c1781edc67873390d21486f7d000904ab8
yindaheng98 <yindaheng98@gmail.com>
2024-10-19 21:39:16 -0700

    only change forward here

--- gaussian_splatting/camera_trainable.py
+++ gaussian_splatting/camera_trainable.py
@@ -1,18 +1,71 @@
-from .gaussian_model import GaussianModel
-from .dataset import TrainableCameraDataset
+import torch
+import math
+from .gaussian_model import GaussianModel, Camera
+
+from gaussian_splatting.diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
+from gaussian_splatting.simple_knn._C import distCUDA2
 
 
 class CameraTrainableGaussianModel(GaussianModel):
-    def __init__(self, dataset: TrainableCameraDataset, *args, **kwargs):
-        self.dataset = dataset
-        super().__init__(*args, **kwargs)
-        for k, v in self.dataset.get_params().items():
-            self.register_parameter(k, v)
 
-    def forward(self, idx: int):
-        camera = self.dataset[idx]
-        return super().forward(camera)
+    def forward(self, viewpoint_camera: Camera):
+        # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+        screenspace_points = torch.zeros_like(self.get_xyz, dtype=self.get_xyz.dtype, requires_grad=True, device=self._xyz.device) + 0
+        try:
+            screenspace_points.retain_grad()
+        except:
+            pass
 
-    def to(self, device):
-        self.dataset.to(device)
-        return super().to(device)
+        # Set up rasterization configuration
+        tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+        tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+        raster_settings = GaussianRasterizationSettings(
+            image_height=int(viewpoint_camera.image_height),
+            image_width=int(viewpoint_camera.image_width),
+            tanfovx=tanfovx,
+            tanfovy=tanfovy,
+            bg=viewpoint_camera.bg_color.to(self._xyz.device),
+            scale_modifier=self.scale_modifier,
+            viewmatrix=viewpoint_camera.world_view_transform,
+            projmatrix=viewpoint_camera.full_proj_transform,
+            sh_degree=self.active_sh_degree,
+            campos=viewpoint_camera.camera_center,
+            prefiltered=False,
+            debug=self.debug,
+            antialiasing=self.antialiasing
+        )
+
+        rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+        means3D = self.get_xyz
+        means2D = screenspace_points
+        opacity = self.get_opacity
+
+        scales = self.get_scaling
+        rotations = self.get_rotation
+
+        shs = self.get_features
+
+        # Rasterize visible Gaussians to image, obtain their radii (on screen).
+        rendered_image, radii, depth_image = rasterizer(
+            means3D=means3D,
+            means2D=means2D,
+            shs=shs,
+            colors_precomp=None,
+            opacities=opacity,
+            scales=scales,
+            rotations=rotations,
+            cov3D_precomp=None)
+        rendered_image = viewpoint_camera.postprocess(rendered_image)
+
+        # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+        # They will be excluded from value updates used in the splitting criteria.
+        rendered_image = rendered_image.clamp(0, 1)
+        out = {
+            "render": rendered_image,
+            "viewspace_points": screenspace_points,
+            "visibility_filter": (radii > 0).nonzero(),
+            "radii": radii,
+            "depth": depth_image
+        }
+        return out

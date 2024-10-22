e8b132724aee05ae1bc8f4d28ef4d04b01b1f786
yindaheng98 <yindaheng98@gmail.com>
2024-10-21 22:12:33 -0700

    load camera in render

--- render.py
+++ render.py
@@ -5,6 +5,8 @@ from os import makedirs
 import torchvision
 from argparse import ArgumentParser
 from gaussian_splatting import GaussianModel, CameraTrainableGaussianModel
+from gaussian_splatting.utils import psnr
+from gaussian_splatting.dataset import JSONCameraDataset
 from gaussian_splatting.dataset.colmap import ColmapCameraDataset
 
 parser = ArgumentParser()
@@ -12,21 +14,24 @@ parser.add_argument("--sh_degree", default=3, type=int)
 parser.add_argument("-s", "--source", required=True, type=str)
 parser.add_argument("-d", "--destination", required=True, type=str)
 parser.add_argument("-i", "--iteration", required=True, type=int)
+parser.add_argument("--load_camera", default=None, type=str)
 parser.add_argument("--device", default="cuda", type=str)
 
 
-def main(sh_degree: int, source: str, destination: str, iteration: int, device: str):
+def main(sh_degree: int, source: str, destination: str, iteration: int, load_camera: str, device: str):
     gaussians = GaussianModel(sh_degree).to(device)
     gaussians.load_ply(os.path.join(destination, "point_cloud", "iteration_" + str(iteration), "point_cloud.ply"))
-    dataset = ColmapCameraDataset(source).to(device)
+    dataset = (JSONCameraDataset(load_camera) if load_camera else ColmapCameraDataset(source)).to(device)
     render_path = os.path.join(destination, "ours_{}".format(iteration), "renders")
     gt_path = os.path.join(destination, "ours_{}".format(iteration), "gt")
     makedirs(render_path, exist_ok=True)
     makedirs(gt_path, exist_ok=True)
-    for idx, camera in enumerate(tqdm(dataset, desc="Rendering progress")):
+    pbar = tqdm(dataset, desc="Rendering progress")
+    for idx, camera in enumerate(pbar):
         out = gaussians(camera)
         rendering = out["render"]
         gt = camera.ground_truth_image
+        pbar.set_postfix({"PSNR": psnr(rendering, gt).mean().item()})
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gt_path, '{0:05d}'.format(idx) + ".png"))
 
@@ -34,4 +39,4 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
 if __name__ == "__main__":
     args = parser.parse_args()
     with torch.no_grad():
-        main(args.sh_degree, args.source, args.destination, args.iteration, args.device)
+        main(args.sh_degree, args.source, args.destination, args.iteration, args.load_camera, args.device)

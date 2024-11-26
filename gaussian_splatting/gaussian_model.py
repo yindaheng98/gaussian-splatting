3fc1c33f516e29f49293deb70462f6be19b8b459
yindaheng98 <yindaheng98@gmail.com>
2024-11-26 15:50:25 -0800

    motion_fusion func

--- gaussian_splatting/gaussian_model.py
+++ gaussian_splatting/gaussian_model.py
@@ -250,3 +250,69 @@ class GaussianModel(nn.Module):
 
     def activate_all_sh_degree(self):
         self.active_sh_degree = self.max_sh_degree
+
+    def motion_fusion(self, viewpoint_camera: Camera, motion_map: torch.Tensor):
+        # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+        screenspace_points = torch.zeros_like(self.get_xyz, dtype=self.get_xyz.dtype, requires_grad=True, device=self._xyz.device) + 0
+        try:
+            screenspace_points.retain_grad()
+        except:
+            pass
+
+        # Set up rasterization configuration
+        tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+        tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+        raster_settings = GaussianRasterizationSettings(
+            image_height=int(viewpoint_camera.image_height),
+            image_width=int(viewpoint_camera.image_width),
+            tanfovx=tanfovx,
+            tanfovy=tanfovy,
+            bg=viewpoint_camera.bg_color.to(self._xyz.device),
+            scale_modifier=self.scale_modifier,
+            viewmatrix=viewpoint_camera.world_view_transform,
+            projmatrix=viewpoint_camera.full_proj_transform,
+            sh_degree=self.active_sh_degree,
+            campos=viewpoint_camera.camera_center,
+            prefiltered=False,
+            debug=self.debug,
+            antialiasing=self.antialiasing
+        )
+
+        rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+        means3D = self.get_xyz
+        means2D = screenspace_points
+        opacity = self.get_opacity
+
+        scales = self.get_scaling
+        rotations = self.get_rotation
+
+        shs = self.get_features
+
+        # Rasterize visible Gaussians to image, obtain their radii (on screen).
+        rendered_image, radii, depth_image, transform2d, tran_alpha, tran_det = rasterizer.motion_fusion(
+            motion_map=motion_map,
+            means3D=means3D,
+            means2D=means2D,
+            shs=shs,
+            colors_precomp=None,
+            opacities=opacity,
+            scales=scales,
+            rotations=rotations,
+            cov3D_precomp=None)
+        rendered_image = viewpoint_camera.postprocess(viewpoint_camera, rendered_image)
+
+        # Those Gaussians that were frustum culled or had a radius of 0 were not visible.
+        # They will be excluded from value updates used in the splitting criteria.
+        rendered_image = rendered_image.clamp(0, 1)
+        out = {
+            "render": rendered_image,
+            "viewspace_points": screenspace_points,
+            "visibility_filter": (radii > 0).nonzero(),
+            "radii": radii,
+            "depth": depth_image,
+            "transform2d": transform2d,
+            "tran_alpha": tran_alpha,
+            "tran_det": tran_det
+        }
+        return out

precision bug fixed

render.py
@@ -63,7 +63,7 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
     makedirs(render_path, exist_ok=True)
     makedirs(gt_path, exist_ok=True)
     pbar = tqdm(dataset, desc="Rendering progress")
-    last_eqs, last_radii = None, None
+    last_eqs, last_radii, last_det = None, None, None
     for idx, camera in enumerate(pbar):
         xy_transformed, solution = transform2d_pixel(camera.image_height, camera.image_width, device=device)
         out = gaussians.motion_fusion(camera, xy_transformed)
@@ -73,6 +73,7 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gt_path, '{0:05d}'.format(idx) + ".png"))
 
+        print("\nframe", idx)
         # verify exported data
         valid_idx = (out['radii'] > 0) & (out['tran_det'] > 1e-3)
         B = out['transform2d'][..., 0:6].reshape(-1, 2, 3)[valid_idx]
@@ -94,14 +95,14 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
         if last_eqs is None:
             last_eqs = eqsall
             last_radii = out['radii']
+            last_det = out['tran_det']
             continue
-        visiable_idx = (out['radii'] > 0) & (last_radii > 0)
-        eq2 = torch.cat([last_eqs, eqsall], dim=1)[visiable_idx]
+        eq2 = torch.cat([last_eqs, eqsall], dim=1)
         X, Y = eq2[..., :-1], eq2[..., -1].unsqueeze(-1)
         V11 = X.transpose(1, 2).bmm(X)
         V12 = X.transpose(1, 2).bmm(Y)
         det = torch.linalg.det(V11)
-        valid_idx = det.abs() > 1e-3
+        valid_idx = (out['radii'] > 0) & (last_radii > 0) & (out['tran_det'] > 1e-3) & (last_det.abs() > 1e-3) & (det.abs() > 1e-3)
         sigma_flatten = torch.linalg.inv(V11[valid_idx]).bmm(V12[valid_idx]).squeeze(-1)
         sigma = torch.zeros((sigma_flatten.shape[0], 3, 3), device=sigma_flatten.device)
         sigma[:, 0, 0] = sigma_flatten[:, 0]
@@ -115,16 +116,11 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
         sigma[:, 2, 2] = sigma_flatten[:, 5]
 
         # verify equations
-        B = out['transform2d'][..., 0:6].reshape(-1, 2, 3)[visiable_idx][valid_idx]
-        eqs = out['transform2d'][..., 6:27].reshape(-1, 3, 7)[visiable_idx][valid_idx]
-        conv3D = out['transform2d'][..., 27:36].reshape(-1, 3, 3)[visiable_idx][valid_idx]
-        conv2D = out['transform2d'][..., 36:40].reshape(-1, 2, 2)[visiable_idx][valid_idx]
-        T = out['transform2d'][..., 40:49].reshape(-1, 3, 3)[visiable_idx][valid_idx]
-        conv2D_transformed = torch.zeros((conv2D.shape[0], 2, 2), device=conv2D.device)
-        conv2D_transformed[:, 0, 0] = eqs[..., 0, -1]
-        conv2D_transformed[:, 0, 1] = eqs[..., 1, -1]
-        conv2D_transformed[:, 1, 0] = eqs[..., 1, -1]
-        conv2D_transformed[:, 1, 1] = eqs[..., 2, -1]
+        B = out['transform2d'][..., 0:6].reshape(-1, 2, 3)[valid_idx]
+        eqs = out['transform2d'][..., 6:27].reshape(-1, 3, 7)[valid_idx]
+        conv3D = out['transform2d'][..., 27:36].reshape(-1, 3, 3)[valid_idx]
+        conv2D = out['transform2d'][..., 36:40].reshape(-1, 2, 2)[valid_idx]
+        T = out['transform2d'][..., 40:49].reshape(-1, 3, 3)[valid_idx]
         print((T.bmm(conv3D).bmm(T.transpose(1, 2))[:, :2, :2] - conv2D).abs().mean())
         A2D, b2D = B[..., :-1], B[..., -1]
         conv2D_transformed = torch.zeros((conv2D.shape[0], 2, 2), device=conv2D.device)
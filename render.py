ce9863df357cff2b35634fefa2dd8899ed045941
yindaheng98 <yindaheng98@gmail.com>
2024-12-15 20:40:54 -0800

    organized as functions

--- render.py
+++ render.py
@@ -3,26 +3,14 @@ import os
 from tqdm import tqdm
 from os import makedirs
 import torchvision
-from argparse import ArgumentParser, Namespace
 from gaussian_splatting import GaussianModel, CameraTrainableGaussianModel
-from gaussian_splatting.dataset import TrainableCameraDataset
-from gaussian_splatting.dataset.colmap import ColmapTrainableCameraDataset
+from gaussian_splatting.dataset import CameraDataset, JSONCameraDataset, TrainableCameraDataset
+from gaussian_splatting.dataset.colmap import ColmapCameraDataset, ColmapTrainableCameraDataset
 from gaussian_splatting.utils import psnr
 from lpipsPyTorch import lpips
-from gaussian_splatting.dataset import JSONCameraDataset
-from gaussian_splatting.dataset.colmap import ColmapCameraDataset
-
-parser = ArgumentParser()
-parser.add_argument("--sh_degree", default=3, type=int)
-parser.add_argument("-s", "--source", required=True, type=str)
-parser.add_argument("-d", "--destination", required=True, type=str)
-parser.add_argument("-i", "--iteration", required=True, type=int)
-parser.add_argument("--load_camera", default=None, type=str)
-parser.add_argument("--mode", choices=["pure", "densify", "camera"], default="pure")
-parser.add_argument("--device", default="cuda", type=str)
 
 
-def init_gaussians(sh_degree: int, source: str, device: str, mode: str, load_ply: str, load_camera: str = None):
+def prepare_rendering(sh_degree: int, source: str, device: str, mode: str, load_ply: str, load_camera: str = None):
     match mode:
         case "pure" | "densify":
             gaussians = GaussianModel(sh_degree).to(device)
@@ -37,15 +25,9 @@ def init_gaussians(sh_degree: int, source: str, device: str, mode: str, load_ply
     return dataset, gaussians
 
 
-def main(sh_degree: int, source: str, destination: str, iteration: int, device: str, args):
-    with open(os.path.join(destination, "cfg_args"), 'w') as cfg_log_f:
-        cfg_log_f.write(str(Namespace(sh_degree=sh_degree, source_path=source)))
-    dataset, gaussians = init_gaussians(
-        sh_degree=sh_degree, source=source, device=device, mode=args.mode,
-        load_ply=os.path.join(destination, "point_cloud", "iteration_" + str(iteration), "point_cloud.ply"),
-        load_camera=args.load_camera)
-    render_path = os.path.join(destination, "ours_{}".format(iteration), "renders")
-    gt_path = os.path.join(destination, "ours_{}".format(iteration), "gt")
+def rendering(dataset: CameraDataset, gaussians: GaussianModel, save: str):
+    render_path = os.path.join(save, "renders")
+    gt_path = os.path.join(save, "gt")
     makedirs(render_path, exist_ok=True)
     makedirs(gt_path, exist_ok=True)
     pbar = tqdm(dataset, desc="Rendering progress")
@@ -59,6 +41,20 @@ def main(sh_degree: int, source: str, destination: str, iteration: int, device: 
 
 
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--sh_degree", default=3, type=int)
+    parser.add_argument("-s", "--source", required=True, type=str)
+    parser.add_argument("-d", "--destination", required=True, type=str)
+    parser.add_argument("-i", "--iteration", required=True, type=int)
+    parser.add_argument("--load_camera", default=None, type=str)
+    parser.add_argument("--mode", choices=["pure", "densify", "camera"], default="pure")
+    parser.add_argument("--device", default="cuda", type=str)
     args = parser.parse_args()
+    load_ply = os.path.join(args.destination, "point_cloud", "iteration_" + str(args.iteration), "point_cloud.ply")
+    save = os.path.join(args.destination, "ours_{}".format(args.iteration))
     with torch.no_grad():
-        main(args.sh_degree, args.source, args.destination, args.iteration, args.device, args)
+        dataset, gaussians = prepare_rendering(
+            sh_degree=args.sh_degree, source=args.source, device=args.device, mode=args.mode,
+            load_ply=load_ply, load_camera=args.load_camera)
+        rendering(dataset, gaussians, save)

ad04c83f1dd14724ecbe9b670d4cce09aefee997
yindaheng98 <yindaheng98@gmail.com>
2025-04-16 20:59:27 -0700

    use the API in read_write_model

--- gaussian_splatting/dataset/colmap/dataset.py
+++ gaussian_splatting/dataset/colmap/dataset.py
@@ -8,9 +8,9 @@ from gaussian_splatting import CameraTrainableGaussianModel
 from gaussian_splatting.camera import build_camera
 from gaussian_splatting.dataset import CameraDataset, TrainableCameraDataset
 from gaussian_splatting.utils import focal2fov
-from .utils import (
-    read_extrinsics_text, read_extrinsics_binary,
-    read_intrinsics_text, read_intrinsics_binary,
+from .read_write_model import (
+    read_cameras_text, read_cameras_binary,
+    read_images_text, read_images_binary,
     qvec2rotmat
 )
 
@@ -25,11 +25,11 @@ class ColmapCamera(NamedTuple):
     image_path: str
 
 
-def parse_colmap_camera(cam_extrinsics, cam_intrinsics, image_dir):
-    cameras = []
-    for _, key in enumerate(cam_extrinsics):
-        extr = cam_extrinsics[key]
-        intr = cam_intrinsics[extr.camera_id]
+def parse_colmap_camera(cameras, images, image_dir):
+    parsed_cameras = []
+    for _, key in enumerate(cameras):
+        extr = cameras[key]
+        intr = images[extr.camera_id]
         height = intr.height
         width = intr.width
         R = qvec2rotmat(extr.qvec)
@@ -47,25 +47,13 @@ def parse_colmap_camera(cam_extrinsics, cam_intrinsics, image_dir):
             raise ValueError("Colmap camera model not handled: only undistorted datasets (PINHOLE or SIMPLE_PINHOLE cameras) supported!")
 
         image_path = os.path.join(image_dir, extr.name)
-        cameras.append(ColmapCamera(
+        parsed_cameras.append(ColmapCamera(
             image_height=height, image_width=width,
             R=torch.from_numpy(R), T=torch.from_numpy(T),
             FoVy=FovY, FoVx=FovX,
             image_path=image_path
         ))
-    return cameras
-
-
-def read_cameras_binary(cameras_extrinsic_file, cameras_intrinsic_file, image_dir):
-    cam_extrinsics = read_extrinsics_binary(cameras_extrinsic_file)
-    cam_intrinsics = read_intrinsics_binary(cameras_intrinsic_file)
-    return parse_colmap_camera(cam_extrinsics, cam_intrinsics, image_dir)
-
-
-def read_cameras_text(cameras_extrinsic_file, cameras_intrinsic_file, image_dir):
-    cam_extrinsics = read_extrinsics_text(cameras_extrinsic_file)
-    cam_intrinsics = read_intrinsics_text(cameras_intrinsic_file)
-    return parse_colmap_camera(cam_extrinsics, cam_intrinsics, image_dir)
+    return parsed_cameras
 
 
 def read_colmap_cameras(colmap_folder):
@@ -74,11 +62,14 @@ def read_colmap_cameras(colmap_folder):
     try:
         cameras_extrinsic_file = os.path.join(path, "sparse/0", "images.bin")
         cameras_intrinsic_file = os.path.join(path, "sparse/0", "cameras.bin")
-        return read_cameras_binary(cameras_extrinsic_file, cameras_intrinsic_file, image_dir)
+        cam_extrinsics = read_images_binary(cameras_extrinsic_file)
+        cam_intrinsics = read_cameras_binary(cameras_intrinsic_file)
     except:
         cameras_extrinsic_file = os.path.join(path, "sparse/0", "images.txt")
         cameras_intrinsic_file = os.path.join(path, "sparse/0", "cameras.txt")
-        return read_cameras_text(cameras_extrinsic_file, cameras_intrinsic_file, image_dir)
+        cam_extrinsics = read_images_text(cameras_extrinsic_file)
+        cam_intrinsics = read_cameras_text(cameras_intrinsic_file)
+    return parse_colmap_camera(cam_extrinsics, cam_intrinsics, image_dir)
 
 
 class ColmapCameraDataset(CameraDataset):

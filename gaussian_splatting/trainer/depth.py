b324938638b59a54c6529999c3c9e17e6ef76f99
yindaheng98 <yindaheng98@gmail.com>
2025-04-20 23:37:31 -0700

    sometimes do not need rescale

--- gaussian_splatting/trainer/depth.py
+++ gaussian_splatting/trainer/depth.py
@@ -12,14 +12,16 @@ class DepthTrainer(TrainerWrapper):
     def __init__(
             self, base_trainer: AbstractTrainer,
             depth_from_iter=7500,
-            depth_gt_max=1.0,
+            depth_rescale_gt: bool = True,
+            depth_rescale_gt_max=1.0,
             depth_l1_weight_init=1.0,
             depth_l1_weight_final=0.01,
             depth_l1_weight_max_steps=30_000,
     ):
         super().__init__(base_trainer)
         self.depth_from_iter = depth_from_iter
-        self.depth_gt_max = depth_gt_max
+        self.depth_rescale_gt = depth_rescale_gt
+        self.depth_rescale_gt_max = depth_rescale_gt_max
         self.depth_l1_weight_func = get_expon_lr_func(
             depth_l1_weight_init,
             depth_l1_weight_final,
@@ -36,9 +38,13 @@ class DepthTrainer(TrainerWrapper):
             return loss
         inv_depth = out["depth"].squeeze(0)
         inv_depth_gt = camera.ground_truth_depth
-        mask = inv_depth_gt > (1 / self.depth_gt_max)
-        mean, std = inv_depth[mask].mean(), inv_depth[mask].std()
-        mean_gt, std_gt = inv_depth_gt[mask].mean(), inv_depth_gt[mask].std()
+        if self.depth_rescale_gt:
+            mask = inv_depth_gt > (1 / self.depth_rescale_gt_max)
+            mean_gt, std_gt = inv_depth_gt[mask].mean(), inv_depth_gt[mask].std()
+            mean, std = inv_depth[mask].mean(), inv_depth[mask].std()
+        else:
+            mean_gt, std_gt = inv_depth_gt.mean(), inv_depth_gt.std()
+            mean, std = mean_gt, std_gt
         norm_depth = (inv_depth - mean) / std
         norm_depth_gt = (inv_depth_gt - mean_gt) / std_gt
         depth_l1 = torch.abs((norm_depth - norm_depth_gt)).mean()
@@ -54,7 +60,8 @@ def DepthTrainerWrapper(
         scene_extent: float,
         *args,
         depth_from_iter=7500,
-        depth_gt_max=1.0,
+        depth_rescale_gt: bool = True,
+        depth_rescale_gt_max=1.0,
         depth_l1_weight_init=1.0,
         depth_l1_weight_final=0.01,
         depth_l1_weight_max_steps=30_000,
@@ -62,7 +69,8 @@ def DepthTrainerWrapper(
     return DepthTrainer(
         base_trainer=base_trainer_constructor(model, scene_extent, *args, **kwargs),
         depth_from_iter=depth_from_iter,
-        depth_gt_max=depth_gt_max,
+        depth_rescale_gt=depth_rescale_gt,
+        depth_rescale_gt_max=depth_rescale_gt_max,
         depth_l1_weight_init=depth_l1_weight_init,
         depth_l1_weight_final=depth_l1_weight_final,
         depth_l1_weight_max_steps=depth_l1_weight_max_steps

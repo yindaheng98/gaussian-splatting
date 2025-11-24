20916f9a90ea792f526abe675cb7dd7805d85a8f
yindaheng98 <yindaheng98@gmail.com>
2025-11-23 23:06:09 -0800

    adaptive should in other repo

--- gaussian_splatting/trainer/densifier/__init__.py
+++ gaussian_splatting/trainer/densifier/__init__.py
@@ -1,8 +1,6 @@
 from .abc import AbstractDensifier, DensificationInstruct, DensifierWrapper, NoopDensifier
 from .trainer import DensificationTrainer
 from .densifier import SplitCloneDensifier, SplitCloneDensifierWrapper, SplitCloneDensifierTrainerWrapper
-from .adaptive import AdaptiveSplitCloneDensifier, AdaptiveSplitCloneDensifierWrapper, AdaptiveSplitCloneDensifierTrainerWrapper
 from .pruner import OpacityPruner, OpacityPrunerDensifierWrapper, OpacityPrunerTrainerWrapper
 from .combinations import DensificationTrainerWrapper, BaseDensificationTrainer
-from .combinations import AdaptiveDensificationTrainerWrapper, AdaptiveDensificationTrainer
-from .combinations import FullDensifierWrapper, FullAdaptiveDensifierWrapper
+from .combinations import FullDensifierWrapper
--- gaussian_splatting/trainer/densifier/combinations.py
+++ gaussian_splatting/trainer/densifier/combinations.py
@@ -3,21 +3,12 @@ from gaussian_splatting import GaussianModel
 
 from .abc import AbstractDensifier, NoopDensifier
 from .densifier import SplitCloneDensifierWrapper, SplitCloneDensifierTrainerWrapper
-from .adaptive import AdaptiveSplitCloneDensifierWrapper, AdaptiveSplitCloneDensifierTrainerWrapper
 from .pruner import OpacityPruner, OpacityPrunerDensifierWrapper
 
 
 def FullDensifierWrapper(model: GaussianModel, scene_extent: float, *args, **kwargs) -> AbstractDensifier:
     return OpacityPrunerDensifierWrapper(
         SplitCloneDensifierWrapper,
-        model, scene_extent,
-        *args, **kwargs
-    )
-
-
-def FullAdaptiveDensifierWrapper(model: GaussianModel, scene_extent: float, *args, **kwargs) -> AbstractDensifier:
-    return OpacityPrunerDensifierWrapper(
-        AdaptiveSplitCloneDensifierWrapper,
         model, scene_extent,
         *args, **kwargs
     )
@@ -51,51 +42,11 @@ def DensificationTrainerWrapper(
     )
 
 
-def AdaptiveDensificationTrainerWrapper(
-        noargs_base_densifier_constructor: Callable[[GaussianModel, float], AbstractDensifier],
-        model: GaussianModel,
-        scene_extent: float,
-        *args,
-        prune_from_iter=1000,
-        prune_until_iter=15000,
-        prune_interval=100,
-        prune_screensize_threshold=20,
-        prune_percent_too_big=1,
-        prune_opacity_threshold=0.005,
-        **kwargs):
-    return AdaptiveSplitCloneDensifierTrainerWrapper(
-        lambda model, scene_extent: OpacityPruner(
-            noargs_base_densifier_constructor(model, scene_extent),
-            scene_extent,
-            prune_from_iter=prune_from_iter,
-            prune_until_iter=prune_until_iter,
-            prune_interval=prune_interval,
-            prune_screensize_threshold=prune_screensize_threshold,
-            prune_percent_too_big=prune_percent_too_big,
-            prune_opacity_threshold=prune_opacity_threshold,
-        ),
-        model, scene_extent,
-        *args, **kwargs
-    )
-
-
 def BaseDensificationTrainer(
         model: GaussianModel,
         scene_extent: float,
         *args, **kwargs):
     return DensificationTrainerWrapper(
-        lambda model, scene_extent: NoopDensifier(model),
-        model,
-        scene_extent,
-        *args, **kwargs
-    )
-
-
-def AdaptiveDensificationTrainer(
-        model: GaussianModel,
-        scene_extent: float,
-        *args, **kwargs):
-    return AdaptiveDensificationTrainerWrapper(
         lambda model, scene_extent: NoopDensifier(model),
         model,
         scene_extent,

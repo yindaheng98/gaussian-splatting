41faf45f81d7f0504b7a934bb285dd968a86abc0
yindaheng98 <yindaheng98@gmail.com>
2026-02-07 16:24:36 -0800

    export more useful camera functions

--- gaussian_splatting/__init__.py
+++ gaussian_splatting/__init__.py
@@ -1,3 +1,3 @@
-from .camera import Camera
+from .camera import Camera, camera2dict, build_camera, dict2camera
 from .gaussian_model import GaussianModel
 from .camera_trainable import CameraTrainableGaussianModel
